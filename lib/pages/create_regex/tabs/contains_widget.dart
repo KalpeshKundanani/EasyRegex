26643964afd0067999b706bcf1e25e82da8ba041
Kalpesh <kalpeshbkundanani@gmail.com>
2019-12-29 23:42:02 +0530

    UI change of contains tab to allow user to add multiple parameter for contains

--- lib/pages/create_regex/tabs/contains_widget.dart
+++ lib/pages/create_regex/tabs/contains_widget.dart
@@ -1,186 +1,94 @@
 import 'package:flutter/material.dart';
 
-enum Contains {
-  anything,
-  contains,
-  doesNotContain,
-  none,
+class ContainsWidget extends StatefulWidget {
+  @override
+  _ContainsWidgetState createState() => _ContainsWidgetState();
 }
 
-enum ContainsText {
-  contains1,
-  contains2,
-  contains3,
-}
+class _ContainsWidgetState extends State<ContainsWidget> {
+  Color _accentColor;
 
-const Map<Contains, String> containsNameMap = <Contains, String>{
-  Contains.anything: 'Anything',
-  Contains.contains: 'Contains...',
-  Contains.doesNotContain: 'Does not contain...',
-};
-
-extension ContainsExtention on Contains {
-  String get name => containsNameMap[this];
-}
-
-class ContainsWidget extends StatelessWidget {
-  final ValueNotifier<Contains> checkNotifier =
-      ValueNotifier<Contains>(Contains.anything);
-  final ValueNotifier<Set<ContainsText>> containsCheckNotifier =
-      ValueNotifier<Set<ContainsText>>(<ContainsText>{});
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final accentColor = Theme.of(context).accentColor;
-    return SingleChildScrollView(
-      child: ValueListenableBuilder(
-        valueListenable: containsCheckNotifier,
-        builder: (BuildContext context, value, Widget child) {
-          return ValueListenableBuilder(
-            valueListenable: checkNotifier,
-            builder: (BuildContext context, value, Widget child) {
-              return Column(
-                children: <Widget>[
-                  CheckboxListTile(
-                    dense: true,
-                    activeColor: accentColor,
-                    title: Text(Contains.anything.name),
-                    value: checkNotifier.value == Contains.anything,
-                    onChanged: (bool value) {
-                      checkNotifier.value =
-                          value ? Contains.anything : Contains.none;
-                      containsCheckNotifier.value = <ContainsText>{};
-                    },
-                  ),
-                  CheckboxListTile(
-                    dense: true,
-                    activeColor: accentColor,
-                    title: TextFormField(
-                      cursorColor: accentColor,
-                      decoration: InputDecoration(
-                        focusColor: accentColor,
-                        hoverColor: accentColor,
-                        contentPadding: EdgeInsets.all(16),
-                        hintText: Contains.contains.name,
-                      ),
-                    ),
-                    value: checkNotifier.value == Contains.contains &&
-                        containsCheckNotifier.value
-                            .contains(ContainsText.contains1),
-                    onChanged: (bool value) {
-                      checkNotifier.value = Contains.contains;
-                      final symbols =
-                          Set<ContainsText>.from(containsCheckNotifier.value);
-                      if (value) {
-                        symbols.add(ContainsText.contains1);
-                      } else {
-                        symbols.remove(ContainsText.contains1);
-                      }
-                      containsCheckNotifier.value = symbols;
-                    },
-                  ),
-                  CheckboxListTile(
-                    dense: true,
-                    activeColor: accentColor,
-                    title: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text('Or: '),
-                        Expanded(
-                          child: TextFormField(
-                            cursorColor: accentColor,
-                            decoration: InputDecoration(
-                              focusColor: accentColor,
-                              hoverColor: accentColor,
-                              contentPadding: EdgeInsets.all(16),
-                              hintText: Contains.contains.name,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    value: checkNotifier.value == Contains.contains &&
-                        containsCheckNotifier.value
-                            .contains(ContainsText.contains2),
-                    onChanged: (bool value) {
-                      checkNotifier.value = Contains.contains;
-                      final symbols =
-                          Set<ContainsText>.from(containsCheckNotifier.value);
-                      if (value) {
-                        symbols.add(ContainsText.contains2);
-                      } else {
-                        symbols.remove(ContainsText.contains2);
-                      }
-                      containsCheckNotifier.value = symbols;
-                    },
-                  ),
-                  CheckboxListTile(
-                    dense: true,
-                    activeColor: accentColor,
-                    title: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text('Or: '),
-                        Expanded(
-                          child: TextFormField(
-                            cursorColor: accentColor,
-                            decoration: InputDecoration(
-                              focusColor: accentColor,
-                              hoverColor: accentColor,
-                              contentPadding: EdgeInsets.all(16),
-                              hintText: Contains.contains.name,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    value: checkNotifier.value == Contains.contains &&
-                        containsCheckNotifier.value
-                            .contains(ContainsText.contains3),
-                    onChanged: (bool value) {
-                      checkNotifier.value = Contains.contains;
-                      final symbols =
-                          Set<ContainsText>.from(containsCheckNotifier.value);
-                      if (value) {
-                        symbols.add(ContainsText.contains3);
-                      } else {
-                        symbols.remove(ContainsText.contains3);
-                      }
-                      containsCheckNotifier.value = symbols;
-                    },
-                  ),
-                  CheckboxListTile(
-                    dense: true,
-                    activeColor: accentColor,
-                    title: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        Text('But: '),
-                        Expanded(
-                          child: TextFormField(
-                            cursorColor: accentColor,
-                            decoration: InputDecoration(
-                              focusColor: accentColor,
-                              hoverColor: accentColor,
-                              contentPadding: EdgeInsets.all(16),
-                              hintText: Contains.doesNotContain.name,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    value: checkNotifier.value == Contains.doesNotContain,
-                    onChanged: (bool value) {
-                      checkNotifier.value =
-                          value ? Contains.doesNotContain : Contains.none;
-                    },
-                  ),
-                ],
-              );
-            },
-          );
-        },
-      ),
+    _accentColor = Theme.of(context).accentColor;
+    return ListView(
+      children: <Widget>[
+        CheckboxListTile(
+          title: Text('Anything'),
+          dense: true,
+          activeColor: _accentColor,
+          value: true,
+          onChanged: (bool value) {},
+        ),
+        ParameterListCreator(
+          title: 'Contains...',
+          onPressed: () {},
+        ),
+        ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          itemCount: 5,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            return CheckboxListTile(
+              title: Text(index.toString()),
+              value: true,
+              onChanged: (bool value) {},
+            );
+          },
+        ),
+        ParameterListCreator(
+          title: 'But doesn\'t contain...',
+          onPressed: () {},
+        ),
+        ListView.builder(
+          itemCount: 5,
+          shrinkWrap: true,
+          itemBuilder: (BuildContext context, int index) {
+            return Text(index.toString());
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class ParameterListCreator extends StatelessWidget {
+  final String title;
+  final VoidCallback onPressed;
+
+  const ParameterListCreator(
+      {Key key, @required this.title, @required this.onPressed})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _accentColor = Theme.of(context).accentColor;
+    return Column(
+      children: <Widget>[
+        Padding(
+          padding: const EdgeInsets.only(right: 16.0, left: 16.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text(title),
+              IconButton(
+                icon: Icon(Icons.add),
+                onPressed: onPressed,
+              ),
+            ],
+          ),
+        ),
+        Divider(
+          color: _accentColor,
+          thickness: 1,
+          endIndent: 0,
+        ),
+      ],
     );
   }
 }
